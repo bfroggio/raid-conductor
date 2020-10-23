7be11597a41ce466bb7e81162c0848d1f3f11b7d
Jesse Millar <jesse.millar@microsoft.com>
2020-10-23 14:08:00 -0500

    Starting to work

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"errors"
 	"log"
 
 	"github.com/nicklaw5/helix"
@@ -9,6 +9,37 @@ import (
 )
 
 func main() {
+	client, err := configureClient()
+	if err != nil {
+		log.Fatal("Error configuring client:", err.Error())
+	}
+
+	// Get banned game IDs once at the top of main to preserve API calls and decrease latency
+	bannedGames, err := getBannedGameIDs(client)
+	if err != nil {
+		log.Fatal("Error getting banned game IDs:", err.Error())
+	}
+
+	for _, channel := range viper.GetStringSlice("priority_streamers") {
+		if isChannelRaidCandidate(client, channel, bannedGames) {
+			log.Println("Potentially raid: " + channel)
+		}
+	}
+}
+
+func readConfigFile() error {
+	viper.SetConfigName("config") // name of config file (without extension)
+	viper.SetConfigType("toml")   // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath(".")      // optionally look for config in the working directory
+	err := viper.ReadInConfig()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func configureClient() (*helix.Client, error) {
 	err := readConfigFile()
 	if err != nil {
 		log.Fatal("Could not read config file:", err.Error())
@@ -30,46 +61,61 @@ func main() {
 	// Set the access token on the client
 	client.SetAppAccessToken(tokenResponse.Data.AccessToken)
 
+	return client, nil
+
+}
+
+func getBannedGameIDs(client *helix.Client) (*helix.GamesResponse, error) {
+	resp, err := client.GetGames(&helix.GamesParams{
+		Names: viper.GetStringSlice("games_blacklist"),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func isChannelRaidCandidate(client *helix.Client, channel string, bannedGames *helix.GamesResponse) bool {
+	channelStatus, err := getChannelStatus(client, channel)
+	if err != nil {
+		log.Fatal("Error getting channel info:", err.Error())
+	}
+
+	// Is channel live?
+	if !channelStatus.IsLive {
+		return false
+	}
+
 	/*
-		resp, err := client.GetUsers(&helix.UsersParams{
-			Logins: []string{"summit1g", "lirik"},
-		})
-		if err != nil {
-			log.Fatal("Error getting users:", err.Error())
-		}
-
-		fmt.Printf("Status code: %d\n", resp.StatusCode)
-		fmt.Println("Error message: " + resp.ErrorMessage)
-		fmt.Printf("Rate limit: %d\n", resp.GetRateLimit())
-		fmt.Printf("Rate limit remaining: %d\n", resp.GetRateLimitRemaining())
-		fmt.Printf("Rate limit reset: %d\n\n", resp.GetRateLimitReset())
-
-		for _, user := range resp.Data.Users {
-			fmt.Printf("ID: %s Name: %s\n", user.ID, user.DisplayName)
+		// TODO Is channel in preferred language?
+		if !channelStatus.IsLive {
+			return false
 		}
 	*/
 
+	// TODO Is channel streaming a banned game?
+	for _, game := range bannedGames.Data.Games {
+		if channelStatus.GameID == game.ID {
+			return false
+		}
+	}
+
+	return true
+}
+
+func getChannelStatus(client *helix.Client, channel string) (helix.Channel, error) {
 	resp, err := client.SearchChannels(&helix.SearchChannelsParams{
-		Channel: "bfroggio",
-		First:   1,
+		Channel: channel,
+		First:   1, // We only want the topmost result (ideally a perfect match)
 	})
 	if err != nil {
 		log.Fatal("Error searching streams:", err.Error())
 	}
 
-	for _, channel := range resp.Data.Channels {
-		fmt.Printf("Channel: %s; Live: %t; Language: %s\n", channel.DisplayName, channel.IsLive, channel.Language)
-	}
-}
-
-func readConfigFile() error {
-	viper.SetConfigName("config") // name of config file (without extension)
-	viper.SetConfigType("toml")   // REQUIRED if the config file does not have the extension in the name
-	viper.AddConfigPath(".")      // optionally look for config in the working directory
-	err := viper.ReadInConfig()
-	if err != nil {
-		return err
+	if len(resp.Data.Channels) > 0 {
+		return resp.Data.Channels[0], nil
 	}
 
-	return nil
+	return helix.Channel{}, errors.New("No channels found that matched: " + channel)
 }
